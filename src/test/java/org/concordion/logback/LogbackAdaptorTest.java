0d366a0c64778a71b805b94e636fe559138f0843
Andrew Sumner <andrew.sumner@xtra.co.nz>
2017-07-11 00:12:31 +1200

    Failing test to prove problem with logback adaptor

--- src/test/java/org/concordion/logback/LogbackAdaptorTest.java
+++ src/test/java/org/concordion/logback/LogbackAdaptorTest.java
@@ -1,5 +1,9 @@
 package org.concordion.logback;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -9,8 +13,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 import org.concordion.logback.LogbackAdaptor;
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.assertThat;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -57,16 +59,31 @@ public class LogbackAdaptorTest {
 		public Boolean call() throws Exception {
 			LogbackAdaptor lba = new LogbackAdaptor();
 
-			String logfile = "/users/andrew/Documents/log" + thread + ".log";
+			String logfile = "/users/andrew/Documents/thread_" + thread +"_";
 			lba.startLogFile(logfile);
 
 			for (int i = 0; i < 10; i++) { 
 				// TODO test without debug as that caused another problem in LogbackAdaptor
 				logger.debug("This is thread " + thread + " attempt " + String.valueOf(i));
 		
-				assertThat(lba.getLogFile(), equalTo(logfile));
+// TODO Expecting to occasionally get java.util.ConcurrentModificationException at java.util.LinkedHashMap$LinkedHashIterator.nextNode (LinkedHashMap.java:711)
+// when calling getLogFile().  Need to fix this.
+//				
+// 	Sometimes this test gets following exception.  Why?  I've never seen it at MSD but not to say it hasn't happened.
+//				Caused by: java.lang.NullPointerException
+//				at org.concordion.logback.LogbackAdaptorTest$MyRunnable.call(LogbackAdaptorTest.java:71)
+//				at org.concordion.logback.LogbackAdaptorTest$MyRunnable.call(LogbackAdaptorTest.java:1)
+//				at java.util.concurrent.FutureTask.run(FutureTask.java:266)
+//				at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1142)
+//				at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:617)
+//				at java.lang.Thread.run(Thread.java:748)
+				assertThat(lba.getLogFile().getAbsolutePath(), startsWith(logfile));
 			}
 			
+	
+
+
+
 			return true;
 		}
 	}
